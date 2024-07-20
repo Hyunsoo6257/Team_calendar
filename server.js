8a74e2fe89603b2a4737ec44f87669fa6a413339
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-07-20 23:15:57 +1000

    connect Front end

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ require("reflect-metadata");
 const express = require("express");
 const { createConnection } = require("typeorm");
 const bodyParser = require("body-parser");
+const path = require("path");
 const setupRoutes = require("./routes/route");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-// Default route
-app.get("/", (req, res) => {
-  res.send("Hello, this is the API server. Use /api for API requests.");
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Catch-all route to serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 // Establish database connection and start the server
